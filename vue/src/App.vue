973e7e717012830925bb0c4bd55ca46c28078ffd
bridgetf7 <fbridget0404@gmail.com>
2023-03-17 00:34:58 -0400

    commit

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,99 +1,17 @@
-<script>
-import { RouterLink, RouterView } from 'vue-router'
-export default {
-  data() {
-    return {
-      products: [
-        {
-          name: "Erica wishes she was this chair and will think of another name",
-          type: "chair",
-          price: 450,
-          img: "https://unsplash.com/photos/UIcBEy5TDTA"
-        },
-        {
-          name: "Erica thought the human was wall decor",
-          type: "chair",
-          price: 800,
-          img: "https://unsplash.com/photos/ej3UoXYMaRI"
-        },
-        {
-          name: "I will name it later—Eroca",
-          type: "dining table",
-          price: 1600,
-          img: "https://unsplash.com/photos/h7HnSTwc7y0"
-        },
-      ]
-    }
-  }
-}
-//https://medium.com/swlh/filtering-sorting-and-searching-in-arrays-with-vue-js-f60951c040fc
-</script>
-
 <template>
-  <h1>buy my stuff :)</h1>
-  <li v-for="product in products">{{ product.name }}</li>
+  <h1>buy my stuff</h1>
+  <ProductCard v-for="product in products" :name="product.name" :product="product" />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<script>
+import ProductCard from './components/ProductCard.vue';
+export default {
+  name: "Products",
+  components: {
+    ProductCard
   }
+}   
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+<style></style>
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ vue/src/components/ProductCard.vue
@@ -0,0 +1,42 @@
+<script setup>
+export default {
+  data() {
+    return {
+      products: [
+        {
+          name: "Erica wishes she was this chair and will think of another name",
+          type: "chair",
+          price: 450,
+          img: "https://unsplash.com/photos/UIcBEy5TDTA"
+        },
+        {
+          name: "Erica thought the human was wall decor",
+          type: "chair",
+          price: 800,
+          img: "https://unsplash.com/photos/ej3UoXYMaRI"
+        },
+        {
+          name: "I will name it later—Eroca",
+          type: "dining table",
+          price: 1600,
+          img: "https://unsplash.com/photos/h7HnSTwc7y0"
+        }
+      ]
+    }
+  }
+}
+</script>
+  
+
+<template>
+  <div>
+    <div class="products">
+      <h2>{{ product.name }}</h2>
+      <img :src="product.image" />
+      <p>{{ product.description }}</p>
+      <p>{{ product.price }}</p>
+    </div>
+  </div>
+</template>
+  
+<style></style>
